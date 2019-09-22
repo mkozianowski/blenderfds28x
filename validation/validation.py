f36bfd13ea49d16f7e44ef583210a60be4e2b829
carlopoletto <carlopoletto@gmail.com>
2019-09-22 18:50:47 +0200

    Initial commit of the validation script

---
+++ validation/validation.py
@@ -0,0 +1,47 @@
+import os
+import bpy
+from pprint import pprint
+
+
+def load_blend(path):
+    print(">> Import")
+    bpy.ops.wm.open_mainfile(filepath=os.path.join(path, 'validation\\cube\\cube.blend'))
+    
+def load_fds(path):
+    print(">> Import")
+    bpy.ops.import_scene.fds(filepath=os.path.join(path, 'validation\\cube\\cube.fds'))
+
+def export_fds(path, file_name):
+    print(">> Export")
+    context = bpy.context
+    sc = context.scene
+    #pprint(type(context))
+    #pprint(dir(context.scene))
+    #pprint(dir(bpy.ops.export_scene.fds))
+    bpy.ops.export_scene.fds(filepath=os.path.join(path, 'validation', file_name))
+
+#==================================================================
+
+PATH_TO_PROJECT = 'F:\\Projects\\VVF\\blenderfds28x' # NB: Da modificare!!!!!!!
+
+# Test 1
+# load from fds file, export to fds file
+try:
+    print("\nTest 1")
+    print("----------------------------")
+    load_fds(PATH_TO_PROJECT)
+    export_fds(PATH_TO_PROJECT, 'test1.fds')
+except:
+    print('\n\nAn error occurred')
+
+# Test 2
+# load from blend file, export to fds file
+try:
+    print("\nTest 2")
+    print("----------------------------")
+    load_blend(PATH_TO_PROJECT)
+    #export_fds(PATH_TO_PROJECT, 'test2.fds')
+except:
+    print('\n\nAn error occurred')
+
+
