6cf059e100088ceb7a3c4c0de30e0e01715dade2
Ruggero <ruggero.poletto@gmail.com>
2019-09-22 22:20:34 +0200

    Salvataggio del file di blender

--- validation/validation.py
+++ validation/validation.py
@@ -18,29 +18,34 @@ def export_fds(path, file_name):
     #pprint(type(context))
     #pprint(dir(context.scene))
     #pprint(dir(bpy.ops.export_scene.fds))
-    bpy.ops.export_scene.fds(filepath=os.path.join(path, 'validation', file_name))
+    #bpy.ops.export_scene.fds(filepath=os.path.join(path, 'validation', file_name))
+    fds_file = sc.to_fds( context=context, full=True)
+    file=open( os.path.join( path, file_name, "w+")
+    file.write( fds_file)
+    file.close()
 
 #==================================================================
 
-PATH_TO_PROJECT = 'F:\\Projects\\VVF\\blenderfds28x' # NB: Da modificare!!!!!!!
+#PATH_TO_PROJECT = 'F:\\Projects\\VVF\\blenderfds28x' # NB: Da modificare!!!!!!!
+#PATH_TO_PROJECT = 'C:\\Users\\rupol\\AppData\\Roaming\\Blender Foundation\\Blender\\2.80\\scripts\\addons\\blenderfds28x'
 
 # Test 1
 # load from fds file, export to fds file
-try:
-    print("\nTest 1")
-    print("----------------------------")
-    load_fds(PATH_TO_PROJECT)
-    export_fds(PATH_TO_PROJECT, 'test1.fds')
-except:
-    print('\n\nAn error occurred')
-
+#try:
+#    print("\nTest 1")
+#    print("----------------------------")
+#    load_fds(PATH_TO_PROJECT)
+#    export_fds(PATH_TO_PROJECT, 'test1.fds')
+#except:
+#    print('\n\nAn error occurred')
+#
 # Test 2
 # load from blend file, export to fds file
 try:
     print("\nTest 2")
     print("----------------------------")
     load_blend(PATH_TO_PROJECT)
-    #export_fds(PATH_TO_PROJECT, 'test2.fds')
+    export_fds(PATH_TO_PROJECT, 'test2.fds')
 except:
     print('\n\nAn error occurred')
 
