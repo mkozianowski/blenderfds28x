789843dcebcb6453a1270770f486150ad2e58b74
carlopoletto <carlopoletto@gmail.com>
2019-10-06 17:44:25 +0200

    Sviluppo algoritmo loop cartelle di test

--- validation/validation.py
+++ validation/validation.py
@@ -1,63 +1,62 @@
 import os
 import bpy
 from pprint import pprint
-from blenderfds28x import types
-from blenderfds28x import utils
 
 
-def load_blend(path):
-    print(">> Import")
-    bpy.ops.wm.open_mainfile(filepath=os.path.join(path, 'validation\\cube\\cube.blend'))
+def load_blend(filepath):
+    #print(">> Import")
+    bpy.ops.wm.open_mainfile(filepath=filepath)
     
-def load_fds(path):
-    print(">> Import")
-    bpy.ops.import_scene.fds(filepath=os.path.join(path, 'validation\\cube\\cube.fds'))
+def load_fds(filepath):
+    #print(">> Import")
+    bpy.ops.import_scene.fds(filepath=filepath)
 
 def export_fds(path, file_name):
-    print(">> Export")
+    #print(">> Export")
     context = bpy.context
     sc = context.scene
     #pprint(type(context))
     #pprint(dir(context.scene))
     #pprint(dir(bpy.ops.export_scene.fds))
-    #bpy.ops.export_scene.fds(filepath=os.path.join(path, 'validation', file_name))
-    
-    path = os.path.join(path, 'validation', file_name)
-    try: 
-        fds_file = sc.to_fds( context=context, full=False )
-    except Exception as err:
-        #w.cursor_modal_restore()
-        print ( str( err) + "  pippo" )
-        return {"CANCELLED"}
+    bpy.ops.export_scene.fds(filepath=os.path.join(path, 'validation', file_name))
 
-    print( fds_file )
-    utils.write_to_file( path, fds_file)
+def get_filepath(path, ext):
+    for filename in next(os.walk(path))[2]:
+        if filename.endswith('.' + ext):
+            return os.path.join(path, filename)
 
 #==================================================================
 
-#PATH_TO_PROJECT = 'F:\\Projects\\VVF\\blenderfds28x' # NB: Da modificare!!!!!!!
-PATH_TO_PROJECT = 'C:\\Users\\rupol\\AppData\\Roaming\\Blender Foundation\\Blender\\2.80\\scripts\\addons\\blenderfds28x'
+PATH_TO_PROJECT = '/home/carlo/.config/blender/2.80/scripts/addons/blenderfds28x/' # NB: Da modificare!!!!!!!
 
-# Test 1
-# load from fds file, export to fds file
-#try:
-#    print("\nTest 1")
-#    print("----------------------------")
-#    load_fds(PATH_TO_PROJECT)
-#    export_fds(PATH_TO_PROJECT, 'test1.fds')
-#except:
-#    print('\n\nAn error occurred')
-#
-# Test 2
-# load from blend file, export to fds file
+for dirname in next(os.walk(os.path.join(PATH_TO_PROJECT, 'validation')))[1]:
 
+    print("\n\n" + dirname)
+    print("----------------------------")
+    dirpath = os.path.join(PATH_TO_PROJECT, 'validation', dirname)
 
+    # BLEND TO FDS
+    #-----------------------------------
+    if dirname.startswith('BLN2FDS'):
+        filepath_blend = get_filepath(dirpath, 'blend')
+        filepath_fds   = get_filepath(dirpath, 'fds')
+        if filepath_blend != None and filepath_fds != None:
+            try:
+                load_blend(filepath_blend)
+                #export_fds(PATH_TO_PROJECT, 'test1.fds')
+            except Exception as e:
+                print(e)
 
-print("\nTest 2")
-print("----------------------------")
-#load_blend(PATH_TO_PROJECT)
-export_fds(PATH_TO_PROJECT, 'test2.fds')
-
+    # FDS TO FDS
+    #-----------------------------------
+    elif dirname.startswith('FDS2FDS'):
+        filepath_fds   = get_filepath(dirpath, 'fds')
+        if filepath_fds != None:
+            try:
+                load_fds(filepath_fds)
+                #export_fds(PATH_TO_PROJECT, 'test1.fds')
+            except Exception as e:
+                print(e)
 
 
 
