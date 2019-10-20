34df3140129a7267d787953a5c8efa9856495890
carlopoletto <carlopoletto@gmail.com>
2019-10-20 16:35:18 +0200

    #5 - Utilizzo di file temporanei

--- validation/validation.py
+++ validation/validation.py
@@ -2,6 +2,7 @@ import os
 import sys
 import bpy
 import difflib
+import tempfile
 
 from pprint import pprint
 from xml.dom import minidom
@@ -55,7 +56,7 @@ def compare_fds_files( filea, fileb ):
 
    return [fds_equals, string]
 
-def do_check(checkType, dirpath, pathToExport):
+def do_check(checkType, dirpath):
 
     def get_filepath(path, ext):
         for filename in next(os.walk(path))[2]:
@@ -65,29 +66,37 @@ def do_check(checkType, dirpath, pathToExport):
     result = None
     note   = None
 
-    try:
-        filepath_fds = get_filepath(os.path.join(dirpath, 'FDS_Input_Files'), 'fds')
+    with tempfile.NamedTemporaryFile(suffix='.fds', delete=True) as temporaryFile:
+        try:
+            filepath_fds = get_filepath(os.path.join(dirpath, 'FDS_Input_Files'), 'fds')
 
-        if checkType == "blnfds":
-            filepath_blend = get_filepath(os.path.join(dirpath, 'Blender_Input_Files'), 'blend')
-            bpy.ops.wm.open_mainfile(filepath=filepath_blend)
+            if checkType == "blnfds":
+                filepath_blend = get_filepath(os.path.join(dirpath, 'Blender_Input_Files'), 'blend')
+                bpy.ops.wm.open_mainfile(filepath=filepath_blend)
+                
+            elif checkType == "fdsfds":
+                bpy.ops.import_scene.fds(filepath=filepath_fds)
             
-        elif checkType == "fdsfds":
-            bpy.ops.import_scene.fds(filepath=filepath_fds)
-        
-        else:
-            raise ValueError("Invalid check type")
+            else:
+                raise ValueError("Invalid check type")
 
-        bpy.ops.export_scene.fds(filepath=pathToExport)
-        compare = compare_fds_files(filepath_fds, pathToExport)
-        result = "OK" if compare[0] else "ERROR"
-        note   = compare[1]
-    
-    except Exception as e:
-        result = "EXCEPTION"
-        note   = str(e)
+            bpy.ops.export_scene.fds(filepath=temporaryFile.name)
+            compare = compare_fds_files(filepath_fds, temporaryFile.name)
+            result = "OK" if compare[0] else "ERROR"
+            note   = compare[1]
+        
+        except Exception as e:
+            result = "EXCEPTION"
+            note   = str(e)
+        
+        finally:
+            print("-------------------------------------------------------------------------")
+            print(temporaryFile.name + " - " + result + " - " + note)
+            print("-------------------------------------------------------------------------")
+            #temporaryFile.close()
     
     return [result, note]
+    
 
 def append_case(xml, results, contentName, contentType, contentResult, contentNote):
 
@@ -122,11 +131,12 @@ def append_case(xml, results, contentName, contentType, contentResult, contentNo
 #==================================================================
 
 PATH_TO_VALIDATION = os.path.dirname(os.path.realpath(__file__))
-PATH_TO_EXPORT     = os.path.join(PATH_TO_VALIDATION, 'export.fds')
 PATH_TO_RESULTS    = os.path.join(PATH_TO_VALIDATION, 'results.xml')
 
 RESULT_EMPTY = """<?xml version="1.0"?><testResults></testResults>"""
 DATE_FORMAT = "%d-%m-%Y %H.%M"
+
+xml = None
 
 try:
     print("\n\n")
@@ -167,13 +177,13 @@ try:
         # blnfds
         if blnfds:
             print("> Test: blend to fds")
-            check = do_check("blnfds", dirpath, PATH_TO_EXPORT)
+            check = do_check("blnfds", dirpath)
             append_case(xml, results, dirname, "blnfds", check[0], check[1])
 
         # fdsfds
         if fdsfds:
             print("> Test: fds to fds")
-            check = do_check("fdsfds", dirpath, PATH_TO_EXPORT)
+            check = do_check("fdsfds", dirpath)
             append_case(xml, results, dirname, "fdsfds", check[0], check[1])
         
 finally:
