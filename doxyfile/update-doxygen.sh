af069ca25c685c9ab6a040b49b746001d957c664
Ruggero <ruggero.poletto@gmail.com>
2020-04-05 17:41:38 +0200

    Doxygen comments -> deploy

--- doxyfile/update-doxygen.sh
+++ doxyfile/update-doxygen.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 #generation of new doxygen
-doxygen doxyfile
+echo "Doxygen generation ..."
+doxygen doxyfile > doxygen.log
 
 #moving to branch gh-pages
+echo "Pulling gh-pages branch ..."
 git checkout gh-pages
 git pull
 
 #replacing current doxygen
+echo "Deploying doxygen ..."
 rm -rf  ../docs/html
 mv html ../docs/
 
 #commit
+echo "Commit doxygen ..."
 git add ../docs/html
 COMMITMESSAGE="Doxygen update $(date)"
 git commit -m "$COMMITMESSAGE"
